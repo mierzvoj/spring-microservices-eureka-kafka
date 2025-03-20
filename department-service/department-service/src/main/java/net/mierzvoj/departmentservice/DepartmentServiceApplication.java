a01d408cb70caaa98bef5ef5b09ef5648815fa26
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2025-03-20 17:24:21 +0100

    Add eureka client/server config

--- department-service/department-service/src/main/java/net/mierzvoj/departmentservice/DepartmentServiceApplication.java
+++ department-service/department-service/src/main/java/net/mierzvoj/departmentservice/DepartmentServiceApplication.java
@@ -2,7 +2,7 @@ package net.mierzvoj.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
